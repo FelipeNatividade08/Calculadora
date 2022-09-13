8e81facf8c47c7af189ab098585dc877e115e6e1
Felipe Natividade <79470271+FelipeNatividade08@users.noreply.github.com>
2022-09-13 15:04:03 -0300

    Add files via upload

---
+++ CalculadoraController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projeto_mvc.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    public IActionResult Soma(int valor1, int valor2)
+        {
+            var total = valor1 + valor2;
+            return Ok(total);
+
+
+        }
+    public IActionResult Subtrai(int valor1, int valor2)
+        {
+            var total = valor1 - valor2;
+            return Ok(total);
+
+        }
+
+        public IActionResult Divide(int valor1, int valor2)
+        {
+            var total = valor1 / valor2;
+            return Ok(total);
+
+        }
+                public IActionResult Multiplica(int valor1, int valor2)
+        {
+            var total = valor1 * valor2;
+            return Ok(total);
+
+        }
+    }
+}
