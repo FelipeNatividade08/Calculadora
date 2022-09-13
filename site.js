b1f5e52d34be6e3ecb5e807aee77b4c74ab51ed8
Felipe Natividade <79470271+FelipeNatividade08@users.noreply.github.com>
2022-09-13 15:06:18 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,57 @@
+﻿function Soma() {
+    let valor1 = $('#CampoTela1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculadora/Soma",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    });
+}
+
+function Subtrai() {
+    let valor1 = $('#CampoTela1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculadora/Subtrai",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
+function Divide() {
+    let valor1 = $('#CampoTela1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculadora/Divide",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
+function Multiplica() {
+    let valor1 = $('#CampoTela1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculadora/Multiplica",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
